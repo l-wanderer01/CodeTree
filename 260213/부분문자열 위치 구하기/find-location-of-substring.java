dc734b3ea4c1803e0b70e64726685054ad0670ee
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-02-13 20:52:22 +0900

    Title: 부분문자열 위치 구하기, Time: 124ms, Memory: 10MB, Status: Runtime Error - Codetree

--- 260213/부분문자열 위치 구하기/find-location-of-substring.java
+++ 260213/부분문자열 위치 구하기/find-location-of-substring.java
@@ -10,13 +10,16 @@ public class Main {
         
         int idx = -1; // 인덱스 정보 저장
 
-        for (int i = 0; i < str.length() - target.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == target.charAt(0)) {
                 int len = 1;
                 for (int j = 1; j < target.length(); j++) {
                     if (str.charAt(i + j) == target.charAt(j)) len++;
                 }
-                if (len == target.length()) idx = i;
+                if (len == target.length()) {
+                    idx = i;
+                    break;
+                }
             }
         }
 
