5ffe41eb92e31181ae31b54d6b5700f02c2848ed
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-03-01 14:53:26 +0900

    Title: 문자열 계속 지우기, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 260301/문자열 계속 지우기/keep-removing-string.java
+++ 260301/문자열 계속 지우기/keep-removing-string.java
@@ -7,27 +7,15 @@ public class Main {
         String A = sc.next();
         String B = sc.next();
 
-        boolean flag = true;
-        while (flag) {
-            for (int i = 0; i < A.length(); i++) {
-            // i = 시작 idx
-                if (A.charAt(i) == B.charAt(0)) {
-                    int idx = i + 1;
-                    for (int j = 1; j < B.length(); j++) {
-                        if (i + j >= A.length() || A.charAt(i + j) != B.charAt(j)) {
-                            break;
-                        }
-                        else {
-                            idx++;
-                        }
-                    }
-                    if (idx - i == B.length()) {
-                        A = A.substring(0, i) + A.substring(idx);
-                        break;
-                    }
-                }
-                if (i == A.length() - 1) flag = false;
+        while (true) {
+            
+            int idx = A.indexOf(B);
+
+            if (idx == -1) {
+                break;
             }
+
+            A = A.substring(0, idx) + A.substring(idx + B.length());
         }
 
         System.out.printf("%s", A);
