3b6b3dd5d7d3a2a02d75a7970898d1c6f78475ca
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-03-03 17:23:54 +0900

    Title: 규칙에 따라 밀기, Time: 121ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260303/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        String direct = sc.next();
+
+        int cnt = 0; // 방향 정보 저장 (음수 : L, 양수 : R)
+
+        for (int i = 0; i < direct.length(); i++) {
+            if (direct.charAt(i) == 'L') {
+                cnt--;
+            }
+            else cnt++;
+        }
+
+        if (cnt < 0) {
+            int left = cnt * -1;
+            s = s.substring(left) + s.substring(0, left);
+        }
+        else if (cnt > 0) {
+            int right = cnt;
+            s = s.substring(s.length() - right) + s.substring(0, s.length() - right);
+        }
+
+        System.out.printf("%s", s);
+    }
+}
