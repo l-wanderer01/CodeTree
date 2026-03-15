6c86f6ec72ddde13d06c4166aebf5f6b47a12c83
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-03-15 15:17:45 +0900

    Title: 정수만 추출하기, Time: 123ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260315/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.next();
+        String B = sc.next();
+
+        int A_idx = 0;
+        int B_idx = 0;
+
+        for (int i = 0; i < A.length(); i++) {
+            char tmp = A.charAt(i);
+            if (!Character.isDigit(tmp)) {
+                A_idx = i;
+                break;
+            }
+        }
+        for (int i = 0; i < B.length(); i++) {
+            char tmp = B.charAt(i);
+            if (!Character.isDigit(tmp)) {
+                B_idx = i;
+                break;
+            }
+        }
+
+        String A_num = A.substring(0, A_idx);
+        String B_num = B.substring(0, B_idx);
+
+        int sum = Integer.parseInt(A_num) + Integer.parseInt(B_num);
+        System.out.printf("%d", sum);
+    }
+}
