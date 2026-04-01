8be49f66f3aec874cf345b7928dfb17fe48c8d58
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-04-01 20:13:56 +0900

    Title: 함수를 이용한 369 게임, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260401/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int cnt = 0;
+
+        for (int i = A; i <= B; i++) {
+            if (i % 3 == 0) {
+                cnt++;
+            }
+            else {
+                int num = third(i);
+                if (num == 0) cnt++;
+            }
+        }
+
+        System.out.printf("%d", cnt);
+    }
+
+    private static int third(int n) {
+        if (n < 3) return n;
+        int div = n % 10;
+        if (div % 3 == 0) return 0;
+        return third(n / 10);
+    }
+}
