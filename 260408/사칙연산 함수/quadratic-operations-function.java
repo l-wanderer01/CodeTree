d09439d45b37e734d533d349e8ab2b57c2b678d8
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-04-08 23:41:27 +0900

    Title: 사칙연산 함수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/사칙연산 함수/quadratic-operations-function.java
+++ 260408/사칙연산 함수/quadratic-operations-function.java
@@ -8,8 +8,8 @@ public class Main {
 
         String[] arr = s.split(" ");
 
-        int a = String.parseInt(arr[0]);
-        int b = String.parseInt(arr[2]);
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[2]);
         String cal = arr[1];
         String res = "";
         if (cal.equals("+")) {
@@ -24,24 +24,27 @@ public class Main {
         else if (cal.equals("/")) {
             res = div(a, b);
         }
-        else return "False";
-        s += " " + res;
+        else {
+            System.out.printf("False");
+            return;
+        }
+        s += " = " + res;
         System.out.printf("%s", s);
     }
-}
 
-private static String sum(int a, int b) {
-    return a++"";
-}
+    private static String sum(int a, int b) {
+        return a+b+"";
+    }
 
-private static String minus(int a, int b) {
-    return a-b+"";
-}
+    private static String minus(int a, int b) {
+        return a-b+"";
+    }
 
-private static String mul(int a, int b) {
-    return a*b+"";
-}
+    private static String mul(int a, int b) {
+        return a*b+"";
+    }
 
-private static String div(int a, int b) {
-    return a/b+"";
+    private static String div(int a, int b) {
+        return a/b+"";
+    }
 }
