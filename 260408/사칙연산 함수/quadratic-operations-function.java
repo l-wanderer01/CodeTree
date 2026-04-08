6228f0a78bf01ef87a5c268b9ca61f6d30733632
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-04-08 23:37:46 +0900

    Title: 사칙연산 함수, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260408/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        String[] arr = s.split(" ");
+
+        int a = String.parseInt(arr[0]);
+        int b = String.parseInt(arr[2]);
+        String cal = arr[1];
+        String res = "";
+        if (cal.equals("+")) {
+            res = sum(a, b);
+        }
+        else if (cal.equals("-")) {
+            res = minus(a, b);
+        }
+        else if (cal.equals("*")) {
+            res = mul(a, b);
+        }
+        else if (cal.equals("/")) {
+            res = div(a, b);
+        }
+        else return "False";
+        s += " " + res;
+        System.out.printf("%s", s);
+    }
+}
+
+private static String sum(int a, int b) {
+    return a++"";
+}
+
+private static String minus(int a, int b) {
+    return a-b+"";
+}
+
+private static String mul(int a, int b) {
+    return a*b+"";
+}
+
+private static String div(int a, int b) {
+    return a/b+"";
+}
