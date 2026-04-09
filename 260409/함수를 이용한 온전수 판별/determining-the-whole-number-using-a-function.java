bd98f16a4dff21ffcae10a754912f8d144210cf1
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-04-09 20:30:50 +0900

    Title: 함수를 이용한 온전수 판별, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260409/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int res = 0;
+
+        for (int i = A; i <= B; i++) {
+            if (isPerfectNumber(i)) res++;
+        }
+
+        System.out.printf("%d", res);
+    }
+
+    private static boolean isPerfectNumber(int n) {
+        if (n % 2 != 0 && n % 10 != 5 && n % 3 != 0 || n % 9 == 0) return true;
+        else return false;
+    }
+}
