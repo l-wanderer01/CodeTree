edf8c2086b3344b71608427f0985410fbfb3d4d4
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-04-10 23:57:35 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
+++ 260410/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -9,21 +9,31 @@ public class Main {
         int res = 0;
 
         for (int i = A; i <= B; i++) {
-            if(isPrimeEven(i)) res++;
+            // 소수이면서 자릿수의 합이 짝수인 경우 카운트
+            if (isPrime(i) && isDigitSumEven(i)) {
+                res++;
+            }
         }
 
-        System.out.printf("%d", res);
+        System.out.println(res);
     }
 
-    private static boolean isPrimeEven(int n) {
-        if(((n / 10) + (n % 10)) % 2 == 0) {
-            for (int i = 2; i <= (int)Math.sqrt(n); i++) {
-                if (n % i == 0) return false;
-            }
-            return true;
+    // 소수 판별 함수
+    private static boolean isPrime(int n) {
+        if (n < 2) return false; // 1은 소수가 아님
+        for (int i = 2; i <= (int)Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
-        else{
-            return false;
+        return true;
+    }
+
+    // 모든 자릿수의 합이 짝수인지 판별하는 함수
+    private static boolean isDigitSumEven(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10; // 마지막 자릿수를 더함
+            n /= 10;       // 마지막 자릿수를 제거
         }
+        return sum % 2 == 0;
     }
 }
