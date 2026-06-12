59126a7fe9401cb4782bc925a6cbb748364e922e
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-06-12 23:49:00 +0900

    [Passed] 007 (126ms, 10MB)

---
+++ trail2/007/007.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("secret code : %s\n", sc.next());
+        System.out.printf("meeting point : %s\n", sc.next());
+        System.out.printf("time : %s\n", sc.next());
+    }
+}
