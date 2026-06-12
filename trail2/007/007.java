094e13cbb1567b755b032bc739c7dc84ccc58147
JangJaeHun <129411490+l-wanderer01@users.noreply.github.com>
2026-06-12 23:57:50 +0900

    [Passed] 007 (132ms, 10MB)

--- trail2/007/007.java
+++ trail2/007/007.java
@@ -1,11 +1,28 @@
 import java.util.*;
 
 public class Main {
+
+    private static class StringCode{
+        String secretCode;
+        char meetingPoint;
+        int time;
+
+        private StringCode(String secretCode, char meetingPoint, int time) {
+            this.secretCode = secretCode;
+            this.meetingPoint = meetingPoint;
+            this.time = time;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("secret code : %s\n", sc.next());
-        System.out.printf("meeting point : %s\n", sc.next());
-        System.out.printf("time : %s\n", sc.next());
+        String[] strArr= sc.nextLine().split(" ");
+
+        StringCode strC = new StringCode(strArr[0], strArr[1].charAt(0), Integer.parseInt(strArr[2]));
+
+        System.out.printf("secret code : %s\n", strC.secretCode);
+        System.out.printf("meeting point : %s\n", strC.meetingPoint);
+        System.out.printf("time : %s\n", strC.time);
     }
 }
